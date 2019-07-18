08ee51439d9ec4285f1bbf6d6fde1237d0b46fe6
Elise <elisegamassa@gmail.com>
2019-07-18 14:21:52 +0200

    elise

--- src/Components/NavMain.jsx
+++ src/Components/NavMain.jsx
@@ -4,7 +4,8 @@ import "./NavMain.css";
 import burger from "../images/Burger_King.svg.png";
 // eslint-disable-next-line
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { NavLink } from "react-router-dom";
+
+import { NavLink } from "react-router-dom";
 
 function NavMain() {
   //   const myRef = React.createRef();
@@ -16,28 +17,45 @@ function NavMain() {
   return (
     <div className="navbar">
       <div className="logo">
-        <img className="logo-burger" src={burger} alt="gros logo de burger" />
+        <div className="logo-image">
+          <NavLink to="/">
+            <img
+              className="logo-burger"
+              src={burger}
+              alt="gros logo de burger"
+            />
+          </NavLink>
+        </div>
         <div className="left-logo">
           <h1>GOOD TO GO</h1>
           <p>the fastest way to get to the road</p>
         </div>
+
         <div className="icones">
-          <div className="ico">
-            <i className="fas fa-hamburger fa-2x" />
-            <p>Menu</p>
-          </div>
-          <div className="ico">
-            <i className="fas fa-map-pin fa-2x" />
-            <p>Map</p>
-          </div>
-          <div className="ico">
-            <i className="far fa-user fa-2x" />
-            <p>Log In</p>
-          </div>
-          <div className="ico">
-            <i className="fas fa-sign-in-alt fa-2x" />
-            <p>Sign In</p>
-          </div>
+          <NavLink to="/menu">
+            <div className="ico">
+              <i className="fas fa-hamburger fa-2x" />
+              <p>Menu</p>
+            </div>
+          </NavLink>
+          <NavLink to="/map">
+            <div className="ico">
+              <i className="fas fa-map-pin fa-2x" />
+              <p>Map</p>
+            </div>
+          </NavLink>
+          <NavLink to="/login">
+            <div className="ico">
+              <i className="far fa-user fa-2x" />
+              <p>Log In</p>
+            </div>
+          </NavLink>
+          <NavLink to="/signin">
+            <div className="ico">
+              <i className="fas fa-sign-in-alt fa-2x" />
+              <p>Sign In</p>
+            </div>
+          </NavLink>
         </div>
       </div>
     </div>
