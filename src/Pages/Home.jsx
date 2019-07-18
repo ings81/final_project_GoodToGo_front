bca5c35f343aa54175a796194e94d928356979df
Elise <elisegamassa@gmail.com>
2019-07-18 11:46:30 +0200

    1st commit elise 18/07

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import Map from "../Components/Map";
 
 export default class Home extends Component {
+  handleClick = message => {
+    console.log(message);
+  };
   render() {
     return (
       <div>
         <h1>Home Page</h1>
+        <Map handleClick={this.handleClick} />
       </div>
     );
   }
