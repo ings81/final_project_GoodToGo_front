d1a5992877a9c29a222f82b61deacbc398397d3c
inga Bacher <ingalila@hotmail.fr>
2019-07-23 14:41:07 +0200

    update css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Pages/Home";
 import NavMain from "./Components/NavMain";
 import Payment from "./Pages/Payment";
 import PageAuth from "./Pages/Auth";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faAngleRight, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
