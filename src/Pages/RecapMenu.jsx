a4483bc00a8ce3d51dfde72f50ff0384519202c2
inga Bacher <ingalila@hotmail.fr>
2019-07-25 18:31:47 +0200

    recapmenu updated

--- src/Pages/RecapMenu.jsx
+++ src/Pages/RecapMenu.jsx
@@ -9,20 +9,57 @@ const apiHandler = axios.create({
   withCredentials: true
 });
 
-export default class Recap extends Component {
-  apiHandler.get("/last-order")
-  .then()
-      .catch(err => {
-        this.props.history.push("/signin");
-      });
+class Recap extends Component {
+  state = {
+    purchases: []
+  };
+
+  componentDidMount() {
+    apiHandler
+      .get("/api/users/last-order")
+      .then(res => {
+        console.log(res, "update please ?");
+        const purchases = res.data.purchases;
+        this.setState({
+          purchases,
+          lastOrder: purchases[purchases.length - 1]
+        });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
- 
+    console.log("called first time");
+    if (!this.state.lastOrder) return null;
     return (
       <div>
-        <h1>Récapitulatif de commande</h1>
+        <div>
+          <h2>Recapitulatif de la commande Actuelle</h2>
+          {this.state.lastOrder.articles.map(article => (
+            <div>
+              <p>{article.title}</p>
+              <p>{article.image}</p>
+              <p>{article.price}</p>
+            </div>
+          ))}
+          <p>{this.state.lastOrder.orderRef}</p>
+        </div>
+        <div>
+          <h2>Historique de commandes</h2>
+          {this.state.purchases.map((purchase, i) => (
+            <div>
+              <div>Purchase n°{i + 1}</div>
+              {purchase.articles.map(article => (
+                <div>
+                  <p>{article.title}</p>
+                </div>
+              ))}
+              <p>{purchase.orderRef}</p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
+export default Recap;
