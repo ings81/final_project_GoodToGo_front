9948b471a8f1b75b6a45b4822c1ec4d7a7e53b78
Elise <elisegamassa@gmail.com>
2019-07-25 18:43:01 +0200

    commit

--- src/Pages/RecapMenu.jsx
+++ src/Pages/RecapMenu.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-// import RecapMenu from "../Components/RecapMenu";
-import "../style/Recap.css";
-
-export default class RecapMenu {
-  // handleDelete = index => {
-  //   const selectedMenus = [...this.state.selectedMenus];
-  //   selectedMenus.splice(index, 1);
-  //   this.setState({ selectedMenus });
-  // };
-  render() {
-    return (
-      <div className="recap">
-        <div className="recapborder">
-          <h1>Récapitulatif Commande</h1>
-        </div>
-      </div>
-      /* <div className="recap">
-  <div className ="recapborder">
-  <h1>Récapitulatif Commande</h1><hr/>
-  <div/> */
-=======
 import React, { Component } from "react";
 import ChoiceMenu from "../Components/ChoiceMenu";
 import RecapMenu from "../Components/RecapMenu";
@@ -46,7 +23,6 @@ export default class Recap extends Component {
       <div>
         <h1>Récapitulatif de commande</h1>
       </div>
->>>>>>> 5443ba6a4a39dcb4bc4006bc0ecb0e31ace5a069
     );
   }
 }
